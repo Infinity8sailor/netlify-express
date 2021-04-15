d675c5111e06213a5832b8c2a7bb5783ead3c796
vaibhav Dasharathe <vinfinitysailor@gmail.com>
2021-04-15 19:27:25 +0530

    Update Functionality Revived

--- express/server.js
+++ express/server.js
@@ -123,6 +123,43 @@ router.post("/docs/topics/", function (req, res) {
       // update_data(req.body);
     });
 
+    router.post("/docs/update/", function (req, res) {
+      console.log("res/req",res,req.body);
+      //   res.sendFile('public/topics/'+req.body.fileName+'.json',{ root: __dirname+ '/..' });
+      MongoClient.connect(url, function(err, client) {
+        if (!err) {
+          // Get db
+          const db = client.db(dbName);
+          // Get collection
+          const collection = db.collection('topics');
+          // Find all documents in the collection
+
+          var topic = req.body;
+          if (true){
+              // collection.insertMany([empty_topic_data], function(err, result) {
+              //       assert.equal(err, null); 
+              //       assert.equal(1, result.result.n);
+              //       assert.equal(1, result.ops.length);
+              //       console.log("Inserted 1 document into the collection");
+              // });
+              var topic_id = topic._id;
+              delete topic["_id"];
+              collection.updateOne(
+                { "_id": ObjectId(topic_id)}, { $set: topic },
+                function(err, result) {
+                assert.equal(err, null);
+                assert.equal(1, result.result.n);
+                console.log("Updated the document with the field a equal to 2");
+                callback(result);
+              });
+          };
+            // close db client
+            client.close();
+          }
+        });
+        // update_data(req.body);
+      });
+
   // FUNCTIONS
   
     module.exports = app;
