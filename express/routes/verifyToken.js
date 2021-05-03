7b7d77dee4f90ea72fe8c59ee7721746f7e030ca
vaibhav Dasharathe <vinfinitysailor@gmail.com>
2021-05-03 16:54:02 +0530

    Sepaearate base added

--- express/routes/verifyToken.js
+++ express/routes/verifyToken.js
@@ -8,6 +8,7 @@ const jwt_auth = (req,res,next)=> {
     try {
         const verified = jwt.verify(token, process.env.JWT_KEY);
         req.user = verified;
+        console.log("request Authenticated..");
         next();
     }catch(error) {
         res.status(400).send("Invalid Token");
--- express/server.js
+++ express/server.js
@@ -66,7 +66,7 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 router.post("/docs/topics/", jwt_auth, function (req, res) {
-  console.log("res/req",res,req.body);
+  console.log("res/req",req.body);
   //   res.sendFile('public/topics/'+req.body.fileName+'.json',{ root: __dirname+ '/..' });
   MongoClient.connect(url, function(err, client) {
     if (!err) {
@@ -75,31 +75,44 @@ router.post("/docs/topics/", jwt_auth, function (req, res) {
       // Get collection
       const collection = db.collection('topics');
       // Find all documents in the collection
-      if (req.body.fileName == "Topics"){
-        collection.find({"info":"Topics"}).toArray(function(err, todos) {
+        collection.find({"_id":ObjectId(req.body.fileName[0])}).toArray(function(err, todos) {
           if (!err) {
             // send output back
             res.send(todos[0]);
-            // res.send({"bug":req.body.fileName, "data":todos[0]});
-            console.log(todos[0]);
+            // console.log(todos[0]);
           }
         });
-      }
-      else {
-        collection.find({"name":req.body.fileName}).toArray(function(err, todos) {
-          if (!err) {
-            // send output back
-            res.send(todos[0]);
-            // res.send({"bug1":req.body.fileName, "data1":todos[0]});
-            console.log(todos[0]);
-          }
-        })};
         // close db client
         client.close();
       }
     });
     // update_data(req.body);
   });
+
+router.post("/docs/topicList/", jwt_auth, function (req, res) {
+    console.log("res/req",req.body);
+    //   res.sendFile('public/topics/'+req.body.fileName+'.json',{ root: __dirname+ '/..' });
+    MongoClient.connect(url, function(err, client) {
+      if (!err) {
+        // Get db
+        const db = client.db(dbName);
+        // Get collection
+        const collection = db.collection('users');
+        // Find all documents in the collection
+        collection.find({"email":req.body.user}).toArray(function(err, todos) {
+            if (!err) {
+              // send output back
+
+              res.send(todos[0].topics);
+              console.log(todos[0].topics);
+            }
+          });
+          // close db client
+          client.close();
+        }
+      });
+      // update_data(req.body);
+    });
   
 router.post("/docs/newtopic/", function (req, res) {
     console.log("res/req",res,req.body);
@@ -210,8 +223,8 @@ router.post("/docs/del/", function (req, res) {
       });
       // update_data(req.body);
     });
+
   // FUNCTIONS
-  
     module.exports = app;
     module.exports.handler = serverless(app);
     
