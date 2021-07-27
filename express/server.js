efbddf055deb760cee7046b6d4f8d1f55342aad8
vaibhav dasharathe <vinfinitysailor@gmail.com>
2021-07-27 06:51:01 +0530

    backend updated for non signed users

--- express/server.js
+++ express/server.js
@@ -57,6 +57,35 @@ router.get('/test',jwt_auth, (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+router.post("/guest/topics/", function (req, res) {
+  console.log("from gest topics",req.body);
+  //   res.sendFile('public/topics/'+req.body.fileName+'.json',{ root: __dirname+ '/..' });+
+  if (req.body.fileName[0] == undefined) {
+    res.send("Eroor"); 
+  }
+  else { 
+    MongoClient.connect(url, function(err, client) {
+      if (!err) {
+        // Get db
+        const db = client.db(dbName);
+        // Get collection
+        const collection = db.collection('topics');
+        // Find all documents in the collection
+        collection.find({"_id":ObjectId(req.body.fileName[0])}).toArray(function(err, todos) {
+          if (!err) {
+            // send output back
+            res.send(todos[0]);
+            // console.log(todos[0]);
+          }
+        });
+        // close db client
+        client.close();
+      }
+    });
+  }
+    // update_data(req.body);
+  });
+
 router.post("/docs/topics/", jwt_auth, function (req, res) {
   console.log("res/req",req.body);
   //   res.sendFile('public/topics/'+req.body.fileName+'.json',{ root: __dirname+ '/..' });
@@ -71,7 +100,7 @@ router.post("/docs/topics/", jwt_auth, function (req, res) {
           if (!err) {
             // send output back
             res.send(todos[0]);
-            // console.log(todos[0]);
+            console.log(todos[0]);
           }
         });
         // close db client
@@ -80,17 +109,37 @@ router.post("/docs/topics/", jwt_auth, function (req, res) {
     });
     // update_data(req.body);
   });
-router.get("/guest/topicList", (req,res)=> {
+router.post("/guest/topicList", (req,res)=> {
+  console.log("Guest topicList");
+  // MongoClient.connect(url, function(err, client) {
+  //   if (!err) {
+  //     // Get db
+  //     const db = client.db(dbName);
+  //     // Get collection
+  //     const collection = db.collection('topics');
+  //     // Find all documents in the collection
+  //     collection.find({"info":"Topics"}).toArray(function(err, todos) {
+  //         if (!err) {
+  //           // send output back
+  //           res.send(todos[0].topics);
+  //           console.log(todos[0].topics);
+  //         }
+  //       });
+  //       // close db client
+  //       client.close();
+  //     }
+  //   });
   MongoClient.connect(url, function(err, client) {
     if (!err) {
       // Get db
       const db = client.db(dbName);
       // Get collection
-      const collection = db.collection('topics');
+      const collection = db.collection('users');
       // Find all documents in the collection
-      collection.find({"info":"Topics"}).toArray(function(err, todos) {
+      collection.find({"email":"vinfinitysailor@gmail.com"}).toArray(function(err, todos) {
           if (!err) {
             // send output back
+            if (todos[0] == undefined) return res.status(500).send("doesnt exist");
             res.send(todos[0].topics);
             console.log(todos[0].topics);
           }
